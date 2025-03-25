857c8900d6fe1c645caabafc165e492e2bebdddb
NAVIN0507 <navinofficial2005@gmail.com>
2025-03-25 21:03:46 +0530

    Added 2 section in home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -16,6 +16,18 @@ const page = () => {
     </div>
     <Image src="/robot.png" alt='robo' width={400} height={400} className='max-sm:hidden'/>
    </section>
+   <section className='flex flex-col gap-6 mt-8'>
+    <h2>Your Interviews</h2>
+    <div className='interviews-section'>
+      <p>You haven't taken any interviews yet</p>
+    </div>
+   </section>
+   <section className='flex flex-col gap-6 mt-8'>
+    <h2>Take an Interview</h2>
+    <div className='interviews-section'>
+      <p>There are no interviews available</p>
+    </div>
+   </section>
    </>
   )
 }
