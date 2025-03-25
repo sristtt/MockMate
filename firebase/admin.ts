6c29a42eaf1d0be5990e193ff041fbb73c3d7c20
NAVIN0507 <navinofficial2005@gmail.com>
2025-03-25 22:40:32 +0530

    Added Admin adk , auth , db

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -1,13 +1,22 @@
-import { cert, getApps, initializeApp } from "firebase-admin/app"
-
+import { cert, getApps, initializeApp } from 
+"firebase-admin/app"
+import {getAuth} from "firebase/auth"
+import {getFirestore} from "firebase/firestore"
 const intiFireBaseAdmin = ()=>{
     const apps = getApps();
     if(!apps.length){
         initializeApp({
             credential:cert({
                 projectId:process.env.FIREBASE_PROJECT_ID,
-                clientEmail:
+                clientEmail:process.env.FIREBASE_CLIENT_EMAIL,
+                privateKey:process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g , "\n")
             })
         })
     }
+    return {
+        auth:getAuth(),
+        db:getFirestore()
+    }
 }
+
+export const {auth , db}  =intiFireBaseAdmin()
