8664fdd78a1fb489af7d120ad620365d08e8c05a
NAVIN0507 <navinofficial2005@gmail.com>
2025-03-25 20:55:47 +0530

    Added Hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,20 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>Home Page</div>
+   <>
+   <section className='card-cta'>
+    <div className='flex flex-col gap-6 max-w-lg'>
+      <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+      <p className='text-lg'>Practice on real interview qustions & get instant feedbacks.</p>
+      <Button asChild className='btn-primary max-sm:w-full'>
+        <Link href={"/interview"}>Start an Interview</Link>
+      </Button>
+    </div>
+   </section>
+   </>
   )
 }
 
