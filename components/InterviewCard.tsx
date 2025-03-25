f62a5905256813ad19ebfe73b2ddd0e2bd94a963
NAVIN0507 <navinofficial2005@gmail.com>
2025-03-25 21:47:24 +0530

    Added Image genrator

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import { getRandomInterviewCover } from '@/lib/utils';
 import { Button } from './ui/button';
 import Link from 'next/link';
+import DisplayTechIcons from './DisplayTechIcons';
 
 const InterviewCard = ({interviewId , userId , role , type , techstack , createdAt}:InterviewCardProps) => {
     const feedback = null as Feedback | null;
@@ -35,7 +36,7 @@ const InterviewCard = ({interviewId , userId , role , type , techstack , created
             </p>
         </div>
         <div className='flex flex-row justify-between'>
-            <p>Tech Icons</p>
+          <DisplayTechIcons techStack={techstack}/>
             <Button className='btn-primary'>
                 <Link 
                 href={feedback ? `/interview/${interviewId}/feedback`:`/interview/${interviewId}`}>
