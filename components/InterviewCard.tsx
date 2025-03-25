311223adb597ccf52af111aef984cf29f78bdc2a
NAVIN0507 <navinofficial2005@gmail.com>
2025-03-25 21:26:08 +0530

    Added covers section

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import dayjs from "dayjs"
+import Image from 'next/image';
+import { getRandomInterviewCover } from '@/public/utils';
 
 const InterviewCard = ({interviewId , userId , role , type , techstack , createdAt}:InterviewCardProps) => {
     const feedback = null as Feedback | null;
     const normalizedType = /mix/gi.test(type) ? 'Mixed' : type;
     const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format('MMM D, YYYY');
   return (
-    <div>{formattedDate +  normalizedType}</div>
+   <div className='card-border w-[360px] max-sm:w-full min-h-96'>
+    <div className='card-interview'>
+        <div>
+            <div className='absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg bg-light-400'>
+                <p className='badge-text'>{normalizedType}</p>
+            </div>
+            <Image src={getRandomInterviewCover()} alt='cover image' width={90} height={90} className='rounded-full object-fit size-[90px]'/>
+        </div>
+    </div>
+   </div>
   )
 }
 
