27ee361408d452c2df5c2182a95f4e48054ad8ac
NAVIN0507 <navinofficial2005@gmail.com>
2025-03-26 22:13:23 +0530

    Added sign up server action

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -5,7 +5,18 @@ import { db } from "@/firebase/admin";
 export async function signUp(params : SignUpParams ){
     const {uid , name , email}  = params;
     try {
-        const userRecord  = await db.collection('users').doc(uid).get()
+        const userRecord  = await db.collection('users').doc(uid).get();
+        if(userRecord.exists){
+            return{
+                success:false,
+                message:'User already exists. Please sign in instrd'
+            }
+        }
+
+        await db.collection('users').doc(uid).set({
+            name , email
+        })
+
     } catch (error : any) {
         console.log(error);
         if(error.code === 'auth/email-already-exists'){
