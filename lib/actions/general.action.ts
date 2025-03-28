a6b61923b8b04d674a324f3b9270f724eb28a683
NAVIN0507 <navinofficial2005@gmail.com>
2025-03-28 20:40:30 +0530

    get interview by id

---
+++ lib/actions/general.action.ts
@@ -0,0 +1,35 @@
+"use server"
+import { auth, db } from "@/firebase/admin";
+export async function getInterviewByUserId(userid:string):Promise<Interview[]|null>{
+    const interviews = await db.collection('interviews')
+    .where('userId' , '==' , userid)
+    .orderBy('createdAt' , 'desc')
+    .get()
+
+    return interviews.docs.map((doc)=>({
+        id:doc.id,
+        ...doc.data()
+    }))as Interview[]
+}
+export async function getLatestInterviews(params : GetLatestInterviewsParams):Promise<Interview[]| null>{
+    const {userId , limit=20} = params;
+    const interviews = await db.collection('interviews')
+    .orderBy('createdAt' , 'desc')
+    .where('finalized' , '==' , true)
+    .where('userId' , '!=' , userId)
+    .limit(limit)
+    .get()
+
+    return interviews.docs.map((doc)=>({
+        id:doc.id,
+        ...doc.data()
+    }))as Interview[]
+}
+export async function getInterviewById(id:string):Promise<Interview| null>{
+
+    const interview = await db.collection('interviews')
+    .doc(id)
+    .get()
+
+    return interview.data() as Interview | null
+}
