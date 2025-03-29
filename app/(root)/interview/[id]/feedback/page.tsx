5663d361898ae8d3d2cafc1be9cafe12b263429d
NAVIN0507 <navinofficial2005@gmail.com>
2025-03-29 22:14:31 +0530

    Added Integartion with frontend

--- app/(root)/interview/[id]/feedback/page.tsx
+++ app/(root)/interview/[id]/feedback/page.tsx
@@ -1,6 +1,18 @@
+import { getCurrentUser } from '@/lib/actions/auth.action';
+import { getFeedbackByInterviewId, getInterviewById } from '@/lib/actions/general.action';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const page = () => {
+const page = async({params}:RouteParams) => {
+    const {id} = await params;
+    const user = await getCurrentUser();
+    const interview = await getInterviewById(id);
+    if(!interview) redirect("/");
+    const feedback = await getFeedbackByInterviewId({
+        interviewId:id,
+        userId:user?.id!
+    })
+    console.log(feedback)
   return (
     <div>FeedBack </div>
   )
